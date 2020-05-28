80f8c8b2fb5d23ad8199f60b5980afad33537714
Default Profile <Default Profile>
2020-05-28 21:28:42 +0300

    Initial commit

---
+++ Assets/Scripts/Game2/CubeMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CubeMovement : MonoBehaviour
+{
+    [SerializeField] float moveTime = 0.5f;
+    [SerializeField] float jumpPower = 1f;
+    [SerializeField] float reloadLevelDelay = 1f;
+
+
+    bool allowInput;
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        //TODO spawn particle
+        //TODO play sound
+
+        ScenesLoader.Instance.RestartLevel(reloadLevelDelay);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allowInput = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!allowInput)
+        {
+            //exit
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //new Vector3(0, 0, 1) = Vector3.forward
+            Vector3 newPosition = transform.position + Vector3.forward;
+            MoveTo(newPosition);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 newPosition = transform.position + Vector3.back;
+            MoveTo(newPosition);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 newPosition = transform.position + Vector3.left;
+            MoveTo(newPosition);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 newPosition = transform.position + Vector3.right;
+            MoveTo(newPosition);
+        }
+    }
+
+    void MoveTo(Vector3 newPosition)
+    {
+        //Debug.DrawRay(newPosition, Vector3.down, Color.green, 2f);
+        if (Physics.Raycast(newPosition, Vector3.down, 1f))
+        {
+            allowInput = false;
+            transform.DOJump(newPosition, jumpPower, 1, moveTime)
+                .OnComplete(ResetInput);
+        }
+    }
+
+    void ResetInput()
+    {
+        allowInput = true;
+    }
+}
---
+++ Assets/Scripts/Game2/Spikes.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] float maxYValue = 0.2f;
+    [SerializeField] float moveTime = 1;
+    [SerializeField] float waitTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // StartCoroutine(Movement());
+
+        /*
+        Sequence movementSequence = DOTween.Sequence();
+        movementSequence.AppendInterval(waitTime);
+        movementSequence.Append(transform.DOMoveY(maxYValue, moveTime));
+        movementSequence.AppendCallback(() => PrintUp("Up"));
+        movementSequence.AppendInterval(waitTime);
+        movementSequence.Append(transform.DOMoveY(0, moveTime));
+        movementSequence.AppendCallback(() => 
+            {
+                //callback multiple strings
+            }
+        );
+        movementSequence.SetLoops(-1);
+        */
+
+        Sequence movementSequence = DOTween.Sequence();
+        movementSequence.AppendInterval(waitTime/2)
+            .Append(transform.DOMoveY(maxYValue, moveTime).SetEase(Ease.InExpo))
+            .AppendInterval(waitTime/2)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    void PrintUp(string position)
+    {
+        //print smth
+    }
+
+    IEnumerator Movement()
+    {
+        while(true)
+        {
+            transform.DOMoveY(maxYValue, moveTime);
+            yield return new WaitForSeconds(moveTime + waitTime);
+            transform.DOMoveY(0, moveTime);
+            yield return new WaitForSeconds(moveTime + waitTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CubeMovement cube = other.GetComponent<CubeMovement>();
+        cube.Die();
+    }
+
+}
